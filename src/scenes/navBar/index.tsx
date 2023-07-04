2a0cef0a3013f4a954ff4d27875c363f40ee4f8e
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2023-07-04 16:09:57 -0300

    config files w/ navBar

---
+++ src/scenes/navBar/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from '@/assets/Logo.png'
+
+function navBar(){
+    const FlexBetwenn = "flex items-center justify-between";
+
+    return (
+        <nav>
+            <div
+                className={`${FlexBetwenn} fixed top-0 z-30 w-full py-6`}
+            >
+                <div className={`${FlexBetwenn} mx-auto w-5/6 `} >
+                    <div className={`${FlexBetwenn}`}>
+                        <img src={Logo} alt="Logo" />
+
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default navBar;
